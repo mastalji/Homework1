bac6b1a0a4448de78ab5d96cbed741c51547ac15
JeehoonLee <mastalji@gmail.com>
2024-03-05 15:15:57 +0900

    at1

--- Program.fs
+++ Program.fs
@@ -4,9 +4,26 @@
 /// returns the sum of the squares of the two large numbers. If there is any
 /// error in processing the inputs, e.g., integer overflows, then the function
 /// should simply return -1 (do not raise an exception).
-let prob1 a b c =
-  failwith "TODO"
 
+let prob1 a b c =
+    let square x = x*x
+    try
+        let aInt = int a
+        let bInt = int b
+        let cInt = int c
+        let sum = 
+            if ( aInt > 46341 || aInt < -46341) || ( bInt > 46341 || bInt < -46341) || ( cInt > 46341 || cInt < -46341) then -1
+            elif (aInt >= bInt && bInt >= cInt) || (bInt >= aInt && aInt >= cInt) then
+               square aInt + square bInt
+            elif (aInt >= cInt && cInt >= bInt) || (cInt >= aInt && aInt >= bInt) then 
+               square aInt + square cInt
+            elif ( bInt >= cInt && cInt >= aInt) || ( cInt >=bInt && bInt >= aInt) then
+               square cInt + square bInt 
+            else -1
+        sum        
+    with
+    | :? System.OverflowException -> -1
+    
 /// Define a function `prob2` that takes in a string and returns a new string
 /// that ends with a newline character '\n'. The function appends a newline
 /// character to the given string only if the string does not already end with a
@@ -16,8 +33,11 @@ let prob1 a b c =
 /// Also, one can get the length of a string s by calling a function
 /// String.length: String.length s returns the length of the string s. Finally,
 /// you can append two strings using the + operator.
+
 let prob2 (str: string) =
-  failwith "TODO"
+    if String.length str = 0 then "\n"
+    elif str.[str.Length - 1] <> '\n' then str + "\n"
+    else str
 
 /// Write a function `prob3` that takes in as input three floating point numbers
 /// a, b, and c, and returns a root of the quadratic formula $ax^2 + bx + c =
@@ -26,7 +46,13 @@ let prob2 (str: string) =
 /// there are no real roots, then the function should return nan, which is a
 /// special floating point number representing "Not a Number".
 let prob3 a b c =
-  failwith "TODO"
+    let d = b*b - 4.0*a*c
+    if d > 0.0 then
+        let Dis = System.Math.Sqrt(d)
+        (-b + Dis ) / (2.0*a)
+    elif d = 0.0 then
+        -b / (2.0*a)
+    else nan
 
 /// Define a function `prob4` that returns the number of days in a month. The
 /// function takes in as input an integer representing a month, and outputs the
@@ -34,8 +60,12 @@ let prob3 a b c =
 /// returns -1 for any error cases. For example, if a number big than 12 is
 /// given as input, then the function should return -1.
 let prob4 month =
-  failwith "TODO"
+    match month with 
+    |1 |3 |5 |7 |8 |10 |12 -> 31
+    |4 |6 |9 |11 -> 30
+    |2 -> 28
+    |_ -> -1
 
 [<EntryPoint>]
 let main _args =
-  0
+    0
