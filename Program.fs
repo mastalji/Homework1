d0ed187bdb6c6d50f302b352437bcc202cf90b21
JeehoonLee <161535231+mastalji@users.noreply.github.com>
2024-03-05 14:57:25 +0900

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,41 @@
+﻿module CS220.Program
+
+/// Define a function `prob1` that takes in three (int) numbers as input and
+/// returns the sum of the squares of the two large numbers. If there is any
+/// error in processing the inputs, e.g., integer overflows, then the function
+/// should simply return -1 (do not raise an exception).
+let prob1 a b c =
+  failwith "TODO"
+
+/// Define a function `prob2` that takes in a string and returns a new string
+/// that ends with a newline character '\n'. The function appends a newline
+/// character to the given string only if the string does not already end with a
+/// newline character. Note that a string in F# is indeed, an array of
+/// characters, and each character in a string can be accessed through an item
+/// accessor. For example, str[0] returns the first character of the string str.
+/// Also, one can get the length of a string s by calling a function
+/// String.length: String.length s returns the length of the string s. Finally,
+/// you can append two strings using the + operator.
+let prob2 (str: string) =
+  failwith "TODO"
+
+/// Write a function `prob3` that takes in as input three floating point numbers
+/// a, b, and c, and returns a root of the quadratic formula $ax^2 + bx + c =
+/// 0$. If there are two roots, then the function should return the bigger root.
+/// If there is only one root, then the function should return the root. If
+/// there are no real roots, then the function should return nan, which is a
+/// special floating point number representing "Not a Number".
+let prob3 a b c =
+  failwith "TODO"
+
+/// Define a function `prob4` that returns the number of days in a month. The
+/// function takes in as input an integer representing a month, and outputs the
+/// number of days. You can assume that February has 28 days. The function
+/// returns -1 for any error cases. For example, if a number big than 12 is
+/// given as input, then the function should return -1.
+let prob4 month =
+  failwith "TODO"
+
+[<EntryPoint>]
+let main _args =
+  0
---
+++ Tests/Tests.fs
@@ -0,0 +1,96 @@
+﻿namespace CS220
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+  [<TestMethod; Timeout 1000; TestCategory "1">]
+  member __.``Problem 1.A``() =
+    let r = Program.prob1 1 2 2
+    Assert.AreEqual (8, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "1">]
+  member __.``Problem 1.B``() =
+    let r = Program.prob1 10 1 20
+    Assert.AreEqual (500, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "1">]
+  member __.``Problem 1.C``() =
+    let r = Program.prob1 -1 -1 -1
+    Assert.AreEqual (2, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "1">]
+  member __.``Problem 1.D``() =
+    let r = Program.prob1 0x12345 1 2
+    Assert.AreEqual (-1, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "1">]
+  member __.``Problem 1.E``() =
+    let r = Program.prob1 -0x12345 -0x123456 0x42
+    Assert.AreEqual (-1, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "1">]
+  member __.``Problem 1.F``() =
+    let r = Program.prob1 0xa000 0xd000 0x8000
+    Assert.AreEqual (-1, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "2">]
+  member __.``Problem 2.A``() =
+    let r = Program.prob2 "AAA"
+    Assert.AreEqual ("AAA\n", r)
+
+  [<TestMethod; Timeout 1000; TestCategory "2">]
+  member __.``Problem 2.B``() =
+    let r = Program.prob2 ""
+    Assert.AreEqual ("\n", r)
+
+  [<TestMethod; Timeout 1000; TestCategory "3">]
+  member __.``Problem 3.A``() =
+    let r = Program.prob3 1.0 0.0 0.0
+    Assert.AreEqual (0.0, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "3">]
+  member __.``Problem 3.B``() =
+    let r = Program.prob3 1.1 1.1 1.1
+    Assert.AreEqual (nan, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "3">]
+  member __.``Problem 3.C``() =
+    let r = Program.prob3 1.0 2.0 1.0
+    Assert.AreEqual (-1.0, r, 0.0000001)
+
+  [<TestMethod; Timeout 1000; TestCategory "3">]
+  member __.``Problem 3.D``() =
+    let r = Program.prob3 0.0 0.0 2.0
+    Assert.AreEqual (nan, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "3">]
+  member __.``Problem 3.E``() =
+    let r = Program.prob3 nan nan nan
+    Assert.AreEqual (nan, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "4">]
+  member __.``Problem 4.A``() =
+    let r = Program.prob4 10
+    Assert.AreEqual (31, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "4">]
+  member __.``Problem 4.B``() =
+    let r = Program.prob4 2
+    Assert.AreEqual (28, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "4">]
+  member __.``Problem 4.C``() =
+    let r = Program.prob4 9
+    Assert.AreEqual (30, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "4">]
+  member __.``Problem 4.D``() =
+    let r = Program.prob4 -1
+    Assert.AreEqual (-1, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "4">]
+  member __.``Problem 4.E``() =
+    let r = Program.prob4 0
+    Assert.AreEqual (-1, r)
